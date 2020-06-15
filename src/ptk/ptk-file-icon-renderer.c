zen0bit/spacefm

commit 296979c036f0139b8685fcace7a237970cb7b48b

replace gboolean with bool

src/ptk/ptk-file-icon-renderer.c
@@ -13,6 +13,7 @@
  *
  */
 
+#include <stdbool.h>
 #include <stdint.h>
 
 #include "ptk-file-icon-renderer.h"
@@ -362,7 +363,7 @@ static void ptk_file_icon_renderer_render(GtkCellRenderer* cell, GdkWindow* wind
     GdkRectangle draw_rect;
     VFSFileInfo* file;
     int xpad, ypad;
-    gboolean is_expander, is_expanded;
+    bool is_expander, is_expanded;
 
     GtkCellRendererClass* parent_renderer_class;
 

commit 7210576ba0a802a7c454fafa75044f2ebd620832

wrong type was set, should be int not bool
broken since 296979c036f0139b8685fcace7a237970cb7b48b

fixes same kind of problem as 865a4ffdb92a7a7a1519b06617595d52b6c7fbe3

src/ptk/ptk-file-icon-renderer.c
@@ -343,7 +343,8 @@ static void ptk_file_icon_renderer_render(GtkCellRenderer* cell, GdkWindow* wind
     GdkRectangle draw_rect;
     VFSFileInfo* file;
     int xpad, ypad;
-    bool is_expander, is_expanded;
+    int is_expander;
+    int is_expanded;
 
     GtkCellRendererClass* parent_renderer_class;
 