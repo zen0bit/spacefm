zen0bit/spacefm

commit 8c8e5b85b5ea4fdef938ab86129926771f85ae3c

exo: update deprecated pre-processor symbol

src/exo/exo-icon-view.c
@@ -111,20 +111,11 @@ typedef enum ExoIconViewFlags
         (1l << 1), /* whether current model provides persistent iterators */
 } ExoIconViewFlags;
 
-#define EXO_ICON_VIEW_SET_FLAG(icon_view, flag)          \
-    G_STMT_START                                         \
-    {                                                    \
-        (EXO_ICON_VIEW(icon_view)->priv->flags |= flag); \
-    }                                                    \
-    G_STMT_END
-#define EXO_ICON_VIEW_UNSET_FLAG(icon_view, flag)           \
-    G_STMT_START                                            \
-    {                                                       \
-        (EXO_ICON_VIEW(icon_view)->priv->flags &= ~(flag)); \
-    }                                                       \
-    G_STMT_END
-#define EXO_ICON_VIEW_FLAG_SET(icon_view, flag) \
-    ((EXO_ICON_VIEW(icon_view)->priv->flags & (flag)) == (flag))
+// clang-format off
+#define EXO_ICON_VIEW_SET_FLAG(icon_view, flag)   G_STMT_START{ (EXO_ICON_VIEW (icon_view)->priv->flags |= flag); }G_STMT_END
+#define EXO_ICON_VIEW_UNSET_FLAG(icon_view, flag) G_STMT_START{ (EXO_ICON_VIEW (icon_view)->priv->flags &= ~(flag)); }G_STMT_END
+#define EXO_ICON_VIEW_FLAG_SET(icon_view, flag)   ((EXO_ICON_VIEW (icon_view)->priv->flags & (flag)) == (flag))
+// clang-format on
 
 typedef struct ExoIconViewItem
 {
@@ -304,15 +295,12 @@ typedef struct ExoIconViewPrivate
     unsigned int flags;
 } ExoIconViewPrivate;
 
-#define EXO_ICON_VIEW_GET_PRIVATE(obj) \
-    (G_TYPE_INSTANCE_GET_PRIVATE((obj), EXO_TYPE_ICON_VIEW, ExoIconViewPrivate))
 #define EXO_ICON_VIEW_CELL_INFO(obj) ((ExoIconViewCellInfo*)(obj))
 #define EXO_ICON_VIEW_CHILD(obj)     ((ExoIconViewChild*)(obj))
 #define EXO_ICON_VIEW_ITEM(obj)      ((ExoIconViewItem*)(obj))
 
 static void exo_icon_view_class_init(ExoIconViewClass* klass);
 static void exo_icon_view_cell_layout_init(GtkCellLayoutIface* iface);
-static void exo_icon_view_init(ExoIconView* icon_view);
 static void exo_icon_view_dispose(GObject* object);
 static void exo_icon_view_finalize(GObject* object);
 static void exo_icon_view_get_property(GObject* object, unsigned int prop_id, GValue* value,
@@ -477,31 +465,7 @@ static bool exo_icon_view_search_scroll_event(GtkWidget* widget, GdkEventScroll*
 static bool exo_icon_view_search_timeout(void* user_data);
 static void exo_icon_view_search_timeout_destroy(void* user_data);
 
-static GObjectClass* exo_icon_view_parent_class;
 static unsigned int icon_view_signals[LAST_SIGNAL];
-
-GType exo_icon_view_get_type(void)
-{
-    static GType type = G_TYPE_INVALID;
-
-    if (G_UNLIKELY(type == G_TYPE_INVALID))
-    {
-        type = _exo_g_type_register_simple(GTK_TYPE_CONTAINER,
-                                           "ExoIconView",
-                                           sizeof(ExoIconViewClass),
-                                           exo_icon_view_class_init,
-                                           sizeof(ExoIconView),
-                                           exo_icon_view_init);
-        _exo_g_type_add_interface_simple(type,
-                                         GTK_TYPE_CELL_LAYOUT,
-                                         (GInterfaceInitFunc)exo_icon_view_cell_layout_init);
-#if (GTK_MAJOR_VERSION == 3)
-        _exo_g_type_add_interface_simple(type, GTK_TYPE_SCROLLABLE, NULL);
-#endif
-    }
-
-    return type;
-}
 
 #if (GTK_MAJOR_VERSION == 3)
 static void exo_icon_view_get_preferred_width(GtkWidget* widget, int* minimal_width,
@@ -525,20 +489,17 @@ static void exo_icon_view_get_preferred_height(GtkWidget* widget, int* minimal_h
 }
 #endif
 
+G_DEFINE_TYPE_WITH_CODE(ExoIconView, exo_icon_view, GTK_TYPE_CONTAINER,
+                        G_IMPLEMENT_INTERFACE(GTK_TYPE_CELL_LAYOUT, exo_icon_view_cell_layout_init)
+                        // G_IMPLEMENT_INTERFACE (GTK_TYPE_SCROLLABLE, NULL)
+                        G_ADD_PRIVATE(ExoIconView))
+
 static void exo_icon_view_class_init(ExoIconViewClass* klass)
 {
     GtkContainerClass* gtkcontainer_class;
     GtkWidgetClass* gtkwidget_class;
     GtkBindingSet* gtkbinding_set;
     GObjectClass* gobject_class;
-
-    /* determine the parent type class */
-    exo_icon_view_parent_class = g_type_class_peek_parent(klass);
-
-    /* add our private data to the type's instances */
-    G_GNUC_BEGIN_IGNORE_DEPRECATIONS /* GObject 2.58 */
-        g_type_class_add_private(klass, sizeof(ExoIconViewPrivate));
-    G_GNUC_END_IGNORE_DEPRECATIONS
 
     gobject_class = G_OBJECT_CLASS(klass);
     gobject_class->dispose = exo_icon_view_dispose;
@@ -1196,7 +1157,7 @@ static void exo_icon_view_cell_layout_init(GtkCellLayoutIface* iface)
 
 static void exo_icon_view_init(ExoIconView* icon_view)
 {
-    icon_view->priv = EXO_ICON_VIEW_GET_PRIVATE(icon_view);
+    icon_view->priv = exo_icon_view_get_instance_private(icon_view);
 
     icon_view->priv->selection_mode = GTK_SELECTION_SINGLE;
     icon_view->priv->pressed_button = -1;

src/exo/exo-icon-chooser-dialog.c
@@ -48,9 +48,6 @@
  * or an image file from the local file system.
  **/
 
-#define EXO_ICON_CHOOSER_DIALOG_GET_PRIVATE(obj) \
-    (G_TYPE_INSTANCE_GET_PRIVATE((obj), EXO_TYPE_ICON_CHOOSER_DIALOG, ExoIconChooserDialogPrivate))
-
 static void exo_icon_chooser_dialog_finalize(GObject* object);
 static void exo_icon_chooser_dialog_style_set(GtkWidget* widget, GtkStyle* previous_style);
 static void exo_icon_chooser_dialog_screen_changed(GtkWidget* widget, GdkScreen* previous_screen);
@@ -64,7 +61,7 @@ static bool
 exo_icon_chooser_dialog_start_interactive_search(ExoIconChooserDialog* icon_chooser_dialog);
 static void exo_icon_chooser_dialog_combo_changed(GtkWidget* combo,
                                                   ExoIconChooserDialog* icon_chooser_dialog);
-static void exo_icon_chooser_dialog_entry_changed(GtkWidget* entry,
+static void exo_icon_chooser_dialog_entry_changed(GtkWidget* combo,
                                                   ExoIconChooserDialog* icon_chooser_dialog);
 #if (GTK_MAJOR_VERSION == 2)
 static void exo_icon_chooser_dialog_entry_clear(GtkEntry* entry, GtkEntryIconPosition icon_pos,
@@ -768,7 +765,8 @@ char* exo_icon_chooser_dialog_get_icon(ExoIconChooserDialog* icon_chooser_dialog
  **/
 bool exo_icon_chooser_dialog_set_icon(ExoIconChooserDialog* icon_chooser_dialog, const char* icon)
 {
-    ExoIconChooserDialogPrivate* priv = EXO_ICON_CHOOSER_DIALOG_GET_PRIVATE(icon_chooser_dialog);
+    ExoIconChooserDialogPrivate* priv =
+        exo_icon_chooser_dialog_get_instance_private(icon_chooser_dialog);
     GtkTreeModel* filter;
     GtkTreeModel* model;
     GtkTreePath* filter_path;

src/exo/exo-icon-chooser-dialog.h
@@ -28,11 +28,14 @@ G_BEGIN_DECLS
 
 typedef struct ExoIconChooserDialogPrivate ExoIconChooserDialogPrivate;
 
-#define EXO_TYPE_ICON_CHOOSER_DIALOG (exo_icon_chooser_dialog_get_type())
-#define EXO_ICON_CHOOSER_DIALOG(obj) \
-    (G_TYPE_CHECK_INSTANCE_CAST((obj), EXO_TYPE_ICON_CHOOSER_DIALOG, ExoIconChooserDialog))
-#define EXO_IS_ICON_CHOOSER_DIALOG(obj) \
-    (G_TYPE_CHECK_INSTANCE_TYPE((obj), EXO_TYPE_ICON_CHOOSER_DIALOG))
+// clang-format off
+#define EXO_TYPE_ICON_CHOOSER_DIALOG            (exo_icon_chooser_dialog_get_type ())
+#define EXO_ICON_CHOOSER_DIALOG(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), EXO_TYPE_ICON_CHOOSER_DIALOG, ExoIconChooserDialog))
+#define EXO_ICON_CHOOSER_DIALOG_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), EXO_TYPE_ICON_CHOOSER_DIALOG, ExoIconChooserDialogClass))
+#define EXO_IS_ICON_CHOOSER_DIALOG(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), EXO_TYPE_ICON_CHOOSER_DIALOG))
+#define EXO_IS_ICON_CHOOSER_DIALOG_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), EXO_TYPE_ICON_CHOOSER_DIALOG))
+#define EXO_ICON_CHOOSER_DIALOG_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), EXO_TYPE_ICON_CHOOSER_DIALOG, ExoIconChooserDialogClass))
+// clang-format on
 
 typedef struct ExoIconChooserDialogClass
 {

src/exo/exo-icon-view.h
@@ -36,9 +36,14 @@ typedef struct ExoIconViewPrivate ExoIconViewPrivate;
 typedef struct ExoIconViewClass ExoIconViewClass;
 typedef struct ExoIconView ExoIconView;
 
-#define EXO_TYPE_ICON_VIEW    (exo_icon_view_get_type())
-#define EXO_ICON_VIEW(obj)    (G_TYPE_CHECK_INSTANCE_CAST((obj), EXO_TYPE_ICON_VIEW, ExoIconView))
-#define EXO_IS_ICON_VIEW(obj) (G_TYPE_CHECK_INSTANCE_TYPE((obj), EXO_TYPE_ICON_VIEW))
+// clang-format off
+#define EXO_TYPE_ICON_VIEW            (exo_icon_view_get_type ())
+#define EXO_ICON_VIEW(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), EXO_TYPE_ICON_VIEW, ExoIconView))
+#define EXO_ICON_VIEW_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), EXO_TYPE_ICON_VIEW, ExoIconViewClass))
+#define EXO_IS_ICON_VIEW(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), EXO_TYPE_ICON_VIEW))
+#define EXO_IS_ICON_VIEW_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), EXO_TYPE_ICON_VIEW))
+#define EXO_ICON_VIEW_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), EXO_TYPE_ICON_VIEW, ExoIconViewClass))
+// clang-format on
 
 /**
  * ExoIconViewSearchEqualFunc:

src/exo/exo-tree-view.c
@@ -22,23 +22,18 @@
 
 #include <stdbool.h>
 
+#include <glib-object.h>
 #include <glib/gi18n.h>
+
 #include "exo-tree-view.h"
 #include "exo-string.h"
-#include "exo-marshal.h"
-#include "exo-private.h"
 
-#if defined(G_PARAM_STATIC_NAME) && defined(G_PARAM_STATIC_NICK) && defined(G_PARAM_STATIC_BLURB)
-#define EXO_PARAM_READWRITE \
-    (G_PARAM_READWRITE | G_PARAM_STATIC_NAME | G_PARAM_STATIC_NICK | G_PARAM_STATIC_BLURB)
-#else
-#define EXO_PARAM_READWRITE (G_PARAM_READWRITE)
-#endif
+/* shorter macros for the GParamSpecs with static strings */
+#define EXO_PARAM_READABLE  (G_PARAM_READABLE | G_PARAM_STATIC_STRINGS)
+#define EXO_PARAM_WRITABLE  (G_PARAM_WRITABLE | G_PARAM_STATIC_STRINGS)
+#define EXO_PARAM_READWRITE (G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS)
 
 #define exo_noop_false gtk_false
-
-#define EXO_TREE_VIEW_GET_PRIVATE(obj) \
-    (G_TYPE_INSTANCE_GET_PRIVATE((obj), EXO_TYPE_TREE_VIEW, ExoTreeViewPrivate))
 
 /* Property identifiers */
 enum
@@ -48,8 +43,6 @@ enum
     PROP_SINGLE_CLICK_TIMEOUT,
 };
 
-static void exo_tree_view_class_init(ExoTreeViewClass* klass);
-static void exo_tree_view_init(ExoTreeView* tree_view);
 static void exo_tree_view_finalize(GObject* object);
 static void exo_tree_view_get_property(GObject* object, unsigned int prop_id, GValue* value,
                                        GParamSpec* pspec);
@@ -88,36 +81,13 @@ typedef struct ExoTreeViewPrivate
     GtkTreeViewColumn* activable_column;
 } ExoTreeViewPrivate;
 
-static GObjectClass* exo_tree_view_parent_class;
-
-GType exo_tree_view_get_type(void)
-{
-    static GType type = G_TYPE_INVALID;
-
-    if (G_UNLIKELY(type == G_TYPE_INVALID))
-    {
-        type = _exo_g_type_register_simple(GTK_TYPE_TREE_VIEW,
-                                           "ExoTreeView",
-                                           sizeof(ExoTreeViewClass),
-                                           exo_tree_view_class_init,
-                                           sizeof(ExoTreeView),
-                                           exo_tree_view_init);
-    }
-
-    return type;
-}
+G_DEFINE_TYPE_WITH_PRIVATE(ExoTreeView, exo_tree_view, GTK_TYPE_TREE_VIEW)
 
 static void exo_tree_view_class_init(ExoTreeViewClass* klass)
 {
     GtkTreeViewClass* gtktree_view_class;
     GtkWidgetClass* gtkwidget_class;
     GObjectClass* gobject_class;
-
-    /* add our private data to the class */
-    g_type_class_add_private(klass, sizeof(ExoTreeViewPrivate));
-
-    /* determine our parent type class */
-    exo_tree_view_parent_class = g_type_class_peek_parent(klass);
 
     gobject_class = G_OBJECT_CLASS(klass);
     gobject_class->finalize = exo_tree_view_finalize;
@@ -176,7 +146,7 @@ static void exo_tree_view_class_init(ExoTreeViewClass* klass)
 static void exo_tree_view_init(ExoTreeView* tree_view)
 {
     /* grab a pointer on the private data */
-    tree_view->priv = EXO_TREE_VIEW_GET_PRIVATE(tree_view);
+    tree_view->priv = exo_tree_view_get_instance_private(tree_view);
     tree_view->priv->single_click_timeout_id = -1;
 }
 

src/exo/exo-tree-view.h
@@ -27,20 +27,41 @@
 
 #include <gtk/gtk.h>
 
-G_BEGIN_DECLS;
+G_BEGIN_DECLS
 
 typedef struct ExoTreeViewPrivate ExoTreeViewPrivate;
 
-#define EXO_TYPE_TREE_VIEW    (exo_tree_view_get_type())
-#define EXO_TREE_VIEW(obj)    (G_TYPE_CHECK_INSTANCE_CAST((obj), EXO_TYPE_TREE_VIEW, ExoTreeView))
-#define EXO_IS_TREE_VIEW(obj) (G_TYPE_CHECK_INSTANCE_TYPE((obj), EXO_TYPE_TREE_VIEW))
+// clang-format off
+#define EXO_TYPE_TREE_VIEW            (exo_tree_view_get_type ())
+#define EXO_TREE_VIEW(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), EXO_TYPE_TREE_VIEW, ExoTreeView))
+#define EXO_TREE_VIEW_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), EXO_TYPE_TREE_VIEW, ExoTreeViewClass))
+#define EXO_IS_TREE_VIEW(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), EXO_TYPE_TREE_VIEW))
+#define EXO_IS_TREE_VIEW_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), EXO_TYPE_TREE_VIEW))
+#define EXO_TREE_VIEW_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), EXO_TYPE_TREE_VIEW, ExoTreeViewClass))
+// clang-format on
 
 typedef struct ExoTreeViewClass
 {
     /*< private >*/
     GtkTreeViewClass __parent__;
+
+    /*< private >*/
+    void (*reserved1)(void);
+    void (*reserved2)(void);
+    void (*reserved3)(void);
+    void (*reserved4)(void);
+    void (*reserved5)(void);
+    void (*reserved6)(void);
+    void (*reserved7)(void);
+    void (*reserved8)(void);
 } ExoTreeViewClass;
 
+/**
+ * ExoTreeView:
+ *
+ * The #ExoIconView struct contains only private fields and should
+ * not be directly accessed.
+ **/
 typedef struct ExoTreeView
 {
     /*< private >*/
@@ -68,6 +89,6 @@ void exo_tree_view_set_single_click_timeout(ExoTreeView* tree_view,
 GtkTreeViewColumn* exo_tree_view_get_activable_column(ExoTreeView* tree_view);
 void exo_tree_view_set_activable_column(ExoTreeView* tree_view, GtkTreeViewColumn* column);
 
-G_END_DECLS;
+G_END_DECLS
 
 #endif /* !__EXO_TREE_VIEW_H__ */

commit 7c4797015514ed0f40f1035522703bfd3e27103e

exo: fix broken gtk3 scrolling
broken in
8c8e5b85b5ea4fdef938ab86129926771f85ae3c

src/exo/exo-icon-view.c
@@ -499,8 +499,10 @@ static void exo_icon_view_get_preferred_height(GtkWidget* widget, int* minimal_h
 
 G_DEFINE_TYPE_WITH_CODE(ExoIconView, exo_icon_view, GTK_TYPE_CONTAINER,
                         G_IMPLEMENT_INTERFACE(GTK_TYPE_CELL_LAYOUT, exo_icon_view_cell_layout_init)
-                        // G_IMPLEMENT_INTERFACE (GTK_TYPE_SCROLLABLE, NULL)
-                        G_ADD_PRIVATE(ExoIconView))
+#if (GTK_MAJOR_VERSION == 3)
+                            G_IMPLEMENT_INTERFACE(GTK_TYPE_SCROLLABLE, NULL)
+#endif
+                                G_ADD_PRIVATE(ExoIconView))
 
 static void exo_icon_view_class_init(ExoIconViewClass* klass)
 {